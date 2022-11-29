0de34b29c0ff81cf58371786ce07ea26679f477a
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-11-29 13:17:24 +0100

    Run as sudo

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -e
 ACE_BOX_USER="${ACE_BOX_USER:-$USER}"
 
 # Install required libs incl. ace-cli
-curl -sfL https://raw.githubusercontent.com/tobigremmer-dt/on-the-fly-installer-test/main/cli/ace -o /usr/local/bin/ace
-chmod 0755 /usr/local/bin/ace
+sudo curl -sfL https://raw.githubusercontent.com/tobigremmer-dt/on-the-fly-installer-test/main/cli/ace -o /usr/local/bin/ace
+sudo chmod 0755 /usr/local/bin/ace
 
 # ace prepare
 ace

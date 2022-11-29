055743be5d9995782b87e1266154f4d985265ecd
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-11-29 12:54:48 +0100

    Test user

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Hello there!"
+ACE_BOX_USER="${ACE_BOX_USER:-$USER}"
+
+echo "Hello there, $ACE_BOX_USER!"
+
+# Install required libs incl. ace-cli
+
+# ace prepare
+
+# Conditionally, ace enable $USE_CASE

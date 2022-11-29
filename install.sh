a369747f718e529284d5ce3d69db916aedd0d411
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-11-29 13:12:00 +0100

    Add cli install

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ set -e
 
 ACE_BOX_USER="${ACE_BOX_USER:-$USER}"
 
-echo "Hello there, $ACE_BOX_USER!"
-
 # Install required libs incl. ace-cli
+curl -sfL https://raw.githubusercontent.com/tobigremmer-dt/on-the-fly-installer-test/main/cli/ace -o /usr/local/bin/ace
+chmod 0755 /usr/local/bin/ace
 
 # ace prepare
+ace
 
 # Conditionally, ace enable $USE_CASE

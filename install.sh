1a3e1640cf941147036b8e7a504842189ecf3af0
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-12-16 16:06:11 +0100

    Cleanup install

--- install.sh
+++ install.sh
@@ -21,15 +21,6 @@ python3 -m pip install ansible
 /home/$ACE_BOX_USER/.local/bin/ansible-galaxy collection install https://storage.googleapis.com/ace-box-public-roles/ace_box-operator-0.0.0.tar.gz
 /home/$ACE_BOX_USER/.local/bin/ansible-playbook ace_box.operator.init
 
-# echo "INIT - Installing ACE CLI..."
-# python3 -m pip install https://storage.googleapis.com/ace-box-public-roles/ace_cli-0.0.0-py3-none-any.whl
-
-# # .ace setup
-# echo "INIT - Initializing user ACE-Box..."
-# mkdir -p /home/$ACE_BOX_USER/.ace/ansible
-# curl -sfL $ACE_INVENTORY_SRC/ansible/init.yml -o /home/$ACE_BOX_USER/.ace/ansible/init.yml
-# /home/$ACE_BOX_USER/.local/bin/ansible-playbook /home/$ACE_BOX_USER/.ace/ansible/init.yml --extra-vars "ace_box_user=$ACE_BOX_USER" --extra-vars "ace_inventory_src=$ACE_INVENTORY_SRC"
-
 # # ACE prepare
 # /home/$ACE_BOX_USER/.local/bin/ace prepare
 

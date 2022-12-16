efa51e8f3e11a48c69b4e5bc92c41d348cedb4ad
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-12-16 14:07:58 +0100

    Call ACE CLI with absolute path

--- install.sh
+++ install.sh
@@ -18,17 +18,19 @@ python3 -m pip install --upgrade pip -q
 echo "INIT - Installing Ansible..."
 python3 -m pip install ansible
 
-# .ace setup
-echo "INIT - Initializing user ACE-Box..."
+echo "INIT - Installing ACE CLI..."
 python3 -m pip install https://storage.googleapis.com/ace-box-public-roles/ace_cli-0.0.0-py3-none-any.whl
 
+# .ace setup
+echo "INIT - Initializing user ACE-Box..."
 mkdir -p /home/$ACE_BOX_USER/.ace/ansible
 curl -sfL $ACE_INVENTORY_SRC/ansible/init.yml -o /home/$ACE_BOX_USER/.ace/ansible/init.yml
 /home/$ACE_BOX_USER/.local/bin/ansible-playbook /home/$ACE_BOX_USER/.ace/ansible/init.yml --extra-vars "ace_box_user=$ACE_BOX_USER" --extra-vars "ace_inventory_src=$ACE_INVENTORY_SRC"
 
-ace prepare
+# ACE prepare
+/home/$ACE_BOX_USER/.local/bin/ace prepare
 
 if [ ! -z "${ACE_BOX_USE_CASE}" ]; then
   echo "INIT - Setting up use case..."
-  ace enable $ACE_BOX_USE_CASE
+  /home/$ACE_BOX_USER/.local/bin/ace enable $ACE_BOX_USE_CASE
 fi

89da819f384a811269e9594698f92c5507c7993d
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2022-12-16 16:41:53 +0100

    Add enable step

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 ACE_BOX_USER="${ACE_BOX_USER:-$USER}"
-ACE_INVENTORY_SRC="https://raw.githubusercontent.com/tobigremmer-dt/on-the-fly-installer-test/main"
+ACE_BOX_OPERATOR_SRC="https://storage.googleapis.com/ace-box-public-roles/ace_box-operator-0.0.0.tar.gz"
 
 # Prevent input prompts by specifying frontend is not interactive
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
@@ -18,13 +18,13 @@ python3 -m pip install --upgrade pip -q
 echo "INIT - Installing Ansible..."
 python3 -m pip install ansible
 
-/home/$ACE_BOX_USER/.local/bin/ansible-galaxy collection install https://storage.googleapis.com/ace-box-public-roles/ace_box-operator-0.0.0.tar.gz
+/home/$ACE_BOX_USER/.local/bin/ansible-galaxy collection install $ACE_BOX_OPERATOR_SRC
 /home/$ACE_BOX_USER/.local/bin/ansible-playbook ace_box.operator.init
 
-# # ACE prepare
-# /home/$ACE_BOX_USER/.local/bin/ace prepare
+# ACE prepare
+/home/$ACE_BOX_USER/.local/bin/ace prepare
 
-# if [ ! -z "${ACE_BOX_USE_CASE}" ]; then
-#   echo "INIT - Setting up use case..."
-#   /home/$ACE_BOX_USER/.local/bin/ace enable $ACE_BOX_USE_CASE
-# fi
+if [ ! -z "${ACE_BOX_USE_CASE}" ]; then
+  echo "INIT - Setting up use case..."
+  /home/$ACE_BOX_USER/.local/bin/ace enable $ACE_BOX_USE_CASE
+fi

9410c19b8415335d9f9166a0295512f1487bb605
Tobi Gremmer <tobias.gremmer@dynatrace.com>
2023-01-18 10:53:03 +0100

    Handle error logging; Dynamically handle interval;

--- waitForStatus.sh
+++ waitForStatus.sh
@@ -8,6 +8,8 @@ CUSTOM_TIMEOUT=$2
 TIMEOUT="${CUSTOM_TIMEOUT:-$DEFAULT_TIMEOUT}"
 CURRENT_TIME=$(date +%s)
 TIMEOUT_AT=$(($CURRENT_TIME + $TIMEOUT))
+INTERVAL=10
+CURRENT_INTERVAL=$INTERVAL
 
 #
 # ace_box.operator.init permanently updates PATH in .bashrc.
@@ -17,30 +19,44 @@ TIMEOUT_AT=$(($CURRENT_TIME + $TIMEOUT))
 #
 export PATH=/home/$ACE_BOX_USER/.local/bin:$PATH
 
+getSmallerInterval(){
+  interval_A=$1
+  interval_B=$2
+
+  if [ $interval_A -lt $interval_B ]; then
+    CURRENT_INTERVAL=$interval_A
+  else
+    CURRENT_INTERVAL=$interval_B
+  fi
+}
+
 getStatus () {
   {
-    CURRENT_STATUS=$(ace get status)
-    echo "Current status: $CURRENT_STATUS"
-    return 0
+    CURRENT_STATUS=$(ace get status 2>/dev/null)
   } || {
     echo "ACE CLI not yet available..."
-    return 0
   }
 }
 
 echo "Waiting $TIMEOUT seconds for status: $DESIRED_STATUS"
-getStatus
 
-while [ "$CURRENT_STATUS" != "$DESIRED_STATUS" ]; do
-  sleep 10
-  
-  CURRENT_TIME=$(date +%s)
+while true; do
   getStatus
 
-  if [ $TIMEOUT_AT -lt $CURRENT_TIME ]; then
+  CURRENT_TIME=$(date +%s)
+  TIMEOUT_IN=$(($TIMEOUT_AT - $CURRENT_TIME))
+
+  if [ "$CURRENT_STATUS" = "$DESIRED_STATUS" ]; then
+    break
+  elif [ $TIMEOUT_IN -lt 1 ]; then
     echo "Waiting for status $DESIRED_STATUS timed out!"
     exit 1
+  elif [ ! -z "$CURRENT_STATUS" ]; then
+    echo "Current status: $CURRENT_STATUS. Timeout in $TIMEOUT_IN seconds."
   fi
+
+  getSmallerInterval $INTERVAL $TIMEOUT_IN
+  sleep $CURRENT_INTERVAL
 done
 
-echo "Successfully retrieved desired status: $CURRENT_STATUS"
+echo "Successfully received status: $CURRENT_STATUS"
